995b27951c7e523fe1f2f6f2e6da0b7dc69c96d4
Eledron <eledron@mail.ru>
2024-09-16 16:50:02 +0300

    Step 2

--- students_and_mentor.py
+++ students_and_mentor.py
@@ -7,14 +7,29 @@ class Student:
         self.courses_in_progress = []
         self.grades = {}
 
+    def rate_lecturer(self, lecturer, course, grade):
+        if isinstance(lecturer,
+                      Lecturer) and course in self.courses_in_progress and course in lecturer.courses_attached:
+            if course in lecturer.grades:
+                lecturer.grades[course] += [grade]
+            else:
+                lecturer.grades[course] = [grade]
+        else:
+            return 'Ошибка'
+
+
 class Mentor:
     def __init__(self, name, surname):
         self.name = name
         self.surname = surname
         self.courses_attached = []
 
+
 class Lecturer(Mentor):
-    pass
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
+        self.grades = {}
+
 
 class Reviewer(Mentor):
     def rate_hw(self, student, course, grade):
@@ -27,22 +42,21 @@ class Reviewer(Mentor):
             return 'Ошибка'
 
 
-
-
 best_student = Student('Ruoy', 'Eman', 'your_gender')
 best_student.courses_in_progress += ['Python']
-
 
 cool_lecturer = Lecturer('Some', 'Buddy')
 cool_lecturer.courses_attached += ['Python']
 
-
 cool_reviewer = Reviewer('John', 'Doe')
 cool_reviewer.courses_attached += ['Python']
-
 
 cool_reviewer.rate_hw(best_student, 'Python', 10)
 cool_reviewer.rate_hw(best_student, 'Python', 9)
 cool_reviewer.rate_hw(best_student, 'Python', 8)
 
-print(best_student.grades)
+best_student.rate_lecturer(cool_lecturer, 'Python', 10)
+best_student.rate_lecturer(cool_lecturer, 'Python', 9)
+
+print(f"Оценки студента: {best_student.grades}")
+print(f"Оценки лектора: {cool_lecturer.grades}")
