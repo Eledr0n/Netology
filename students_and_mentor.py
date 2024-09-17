1f75a4fb2e0e25ce8eda034a830db7d06d3be66f
Eledron <eledron@mail.ru>
2024-09-17 13:14:13 +0300

    Step 3

--- students_and_mentor.py
+++ students_and_mentor.py
@@ -133,24 +133,67 @@ class Reviewer(Mentor):
                 f'Фамилия: {self.surname}')
 
 
+student1 = Student('Ruoy', 'Eman', 'your_gender')
+student1.courses_in_progress += ['Python', 'Git']
+student1.finished_courses += ['Введение в программирование']
+student1.grades = {'Python': [10, 9, 8], 'Git': [9, 10]}
 
-best_student = Student('Ruoy', 'Eman', 'your_gender')
-best_student.courses_in_progress += ['Python']
-best_student.finished_courses += ['Введение в программирование']
-best_student.grades = {'Python': [10, 9, 8]}
+student2 = Student('John', 'Doe', 'your_gender')
+student2.courses_in_progress += ['Python']
+student2.finished_courses += ['Основы программирования']
+student2.grades = {'Python': [7, 8, 9]}
+
+lecturer1 = Lecturer('Some', 'Buddy')
+lecturer1.courses_attached += ['Python']
+lecturer1.grades = {'Python': [9, 10]}
+
+lecturer2 = Lecturer('Anna', 'Smith')
+lecturer2.courses_attached += ['Python']
+lecturer2.grades = {'Python': [8, 7, 9]}
+
+reviewer1 = Reviewer('Kate', 'Johnson')
+reviewer1.courses_attached += ['Python']
+
+reviewer2 = Reviewer('Michael', 'Brown')
+reviewer2.courses_attached += ['Python']
+
+reviewer1.rate_hw(student1, 'Python', 10)
+reviewer1.rate_hw(student2, 'Python', 8)
+reviewer2.rate_hw(student1, 'Python', 9)
+reviewer2.rate_hw(student2, 'Python', 7)
+
+student1.rate_lecturer(lecturer1, 'Python', 9)
+student1.rate_lecturer(lecturer2, 'Python', 8)
+student2.rate_lecturer(lecturer1, 'Python', 7)
+student2.rate_lecturer(lecturer2, 'Python', 9)
 
 
-cool_lecturer = Lecturer('Some', 'Buddy')
-cool_lecturer.courses_attached += ['Python']
-cool_lecturer.grades = {'Python': [9, 10]}
+print(student1)
+print(student2)
+print(lecturer1)
+print(lecturer2)
+print(reviewer1)
+print(reviewer2)
 
 
-cool_reviewer = Reviewer('John', 'Doe')
-cool_reviewer.courses_attached += ['Python']
+def average_hw_grade(students, course):
+    total_grades = []
+    for student in students:
+        if course in student.grades:
+            total_grades.extend(student.grades[course])
+    if total_grades:
+        return sum(total_grades) / len(total_grades)
+    return 0
 
-print(best_student)
-print(cool_lecturer)
-print(cool_reviewer)
+def average_lecture_grade(lecturers, course):
+    total_grades = []
+    for lecturer in lecturers:
+        if course in lecturer.grades:
+            total_grades.extend(lecturer.grades[course])
+    if total_grades:
+        return sum(total_grades) / len(total_grades)
+    return 0
 
-print(best_student < Student('Other', 'Student', 'your_gender'))
-print(cool_lecturer > Lecturer('Other', 'Lecturer'))
+
+print(f'Средняя оценка за домашние задания по курсу Python: {average_hw_grade([student1, student2], "Python"):.1f}')
+print(f'Средняя оценка за лекции по курсу Python: {average_lecture_grade([lecturer1, lecturer2], "Python"):.1f}')
